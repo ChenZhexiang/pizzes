5f3b3a5366865a9f1964f558d74cd948cb4c9014
ChenZhexiang <15586948.clot@fje.edu>
2025-02-10 15:02:42 +0000

    Commit 4 de l'aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -1,2 +1,33 @@
-Has demanat només la pizza bàsica. El preu és 9.68 € (IVA inclòs)<br>
-Autor: Chen
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FES LA TEVA PIZZA</title>
+</head>
+<body>
+    <?php
+    $preu = 8;
+    $IVA = 21;
+
+    if (empty($_GET['ingr'])) {
+        $pvp = ((100 + $IVA) / 100) * $preu;
+        echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
+        exit;
+    } else {
+        $pvp = ((100 + $IVA) / 100) * ($preu + count($_GET['ingr']));
+        echo "Has demanat la pizza bàsica amb:<br>";
+        
+        foreach ($_GET['ingr'] as $ingredient) {
+            if (!empty($ingredient)) {
+                echo "* " . htmlspecialchars($ingredient) . "<br>";
+            }
+        }
+        
+        echo "El preu és $pvp € (IVA inclòs)<br>";
+    }
+    ?>
+    <br>
+    <p>Autor: dacomo</p>
+</body>
+</html>
