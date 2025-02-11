e22d0babe1c0d94bf26d63ee0e96a9a5aaa1c9e5
ChenZhexiang <15586948.clot@fje.edu>
2025-02-11 17:56:08 +0000

    Commit 5 de l'aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FES LA TEVA PIZZA</title>
 </head>
-<a href="pizza.html">Retorna al formulari de selecció d’ingredients</a>
 <body>
     <?php
     $preu = 8;
@@ -14,7 +13,7 @@
     if (empty($_GET['ingr'])) {
         $pvp = ((100 + $IVA) / 100) * $preu;
         echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-#        exit;
+#        exit(0);
     } else {
         $pvp = ((100 + $IVA) / 100) * ($preu + count($_GET['ingr']));
         echo "Has demanat la pizza bàsica amb:<br>";
@@ -26,9 +25,11 @@
         }
         
         echo "El preu és $pvp € (IVA inclòs)<br>";
+#	exit(0);
     }
     ?>
     <br>
-    <p>Autor: zhch</p>
+    <p>Autor: ChenZhexiang</p>
+<a href="pizza.html">Retorna al formulari de selecció d’ingredients</a>
 </body>
 </html>
